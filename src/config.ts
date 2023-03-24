a00aeeb57c332a854286eba1d147c94189385617
Bianca Rosca <bianca.rosca@overloop.io>
2023-03-24 16:15:23 +0200

    Change aws region var name

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 const envFormat = z.object({
     PORT: z.coerce.number(),
-    AWS_REGION: z.string(),
+    AWS_BUCKET_REGION: z.string(),
     AWS_ACCESS_KEY_ID: z.string(),
     AWS_SECRET_ACCESS_KEY: z.string(),
 });
