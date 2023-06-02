885156f4c5268e7e03fe6af90ba18cf6c60e41c4
Oliver Wright <owright@theorchard.com>
2023-06-02 12:51:05 +0100

    Cleanup AWS_BUCKET_PATH optionality (#58)

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ export const envFormat = z.object({
     AWS_ACCESS_KEY_ID: z.string().optional(),
     AWS_SECRET_ACCESS_KEY: z.string().optional(),
     AWS_BUCKET_NAME: z.string(),
-    AWS_BUCKET_PATH: z.string(),
+    AWS_BUCKET_PATH: z.string().optional(),
     EXPIRES_IN: z.coerce.number()
 });
 
@@ -24,11 +24,15 @@ if (config.AWS_ACCESS_KEY_ID && config.AWS_SECRET_ACCESS_KEY) {
     };
 }
 
+if (config.AWS_BUCKET_PATH?.length && !config.AWS_BUCKET_PATH.endsWith('/')) {
+    throw new Error('Configuration error: AWS_BUCKET_PATH must end with "/" if specified');
+}
+
 export default {
     PORT: config.PORT,
     AWS_BUCKET_REGION: config.AWS_BUCKET_REGION,
     AWS_BUCKET_NAME: config.AWS_BUCKET_NAME,
-    AWS_BUCKET_PATH: config.AWS_BUCKET_PATH,
+    AWS_BUCKET_PATH: config.AWS_BUCKET_PATH || '',
     EXPIRES_IN: config.EXPIRES_IN,
     AWS_CREDENTIALS: awsCredentials,
 };
