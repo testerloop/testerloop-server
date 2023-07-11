25ea4b6a4759da148676fac38ae2a7803ef92fe1
josephburgess <josephburgess@gmail.com>
2023-07-11 09:50:40 +0100

    add s3config and apikey logic to resolver

--- src/resolvers/Mutation.ts
+++ src/resolvers/Mutation.ts
@@ -1,15 +1,48 @@
 import { v4 as uuidv4 } from 'uuid';
 import { MutationResolvers } from './types/generated';
 import { UploadInfo } from './types/generated';
+import db from '../db.js';
 
 const resolvers: MutationResolvers = {
     createRun: async (
         parent,
-        { customerPath },
-        { dataSources },
+        { runEnvironmentDetails, s3Config },
+        {
+            dataSources,
+            request: {
+                req: { headers },
+            },
+        },
         info
     ): Promise<UploadInfo> => {
         const runID = uuidv4();
+        let organisation;
+        if (headers['api-key']) {
+            const apiKey = headers['api-key'] as string;
+            organisation = await db.organisation.getByApiKey(apiKey);
+
+            if (!organisation) {
+                throw new Error('Invalid API key');
+            }
+        }
+
+        const customerPath =
+            s3Config?.customerPath ?? organisation?.s3CustomPath;
+        const s3BucketName = s3Config?.bucket ?? organisation?.s3BucketName;
+
+        if (!customerPath || !s3BucketName) {
+            throw new Error(
+                'Invalid configuration. Please provide s3BucketName and customerPath.'
+            );
+        }
+
+        await dataSources.createRun.uploadCICDFileToS3(
+            s3BucketName,
+            customerPath,
+            runID,
+            runEnvironmentDetails
+        );
+
         const uploadInfo = await dataSources.createRun.getUploadLink(
             customerPath,
             runID
