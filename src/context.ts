6dacc3c21cc3dbff93f85af40d13373109f6c7e7
josephburgess <josephburgess@gmail.com>
2023-07-12 14:46:30 +0100

    fix npm run build

--- src/context.ts
+++ src/context.ts
@@ -1,6 +1,8 @@
 import { createDataSources, DataSources } from './datasources/index.js';
 import { IncomingHttpHeaders } from 'http';
-
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { LambdaContextFunctionArgument } from '@as-integrations/aws-lambda';
+import { RequestHandler } from '@as-integrations/aws-lambda/dist/request-handlers/_create.js';
 interface Request {
     headers: IncomingHttpHeaders;
 }
@@ -12,7 +14,32 @@ export type Context = {
     };
 };
 
-export const createContext = async ({ req }: { req: Request }): Promise<Context> => {
+export const createContext = async ({
+    event,
+}: LambdaContextFunctionArgument<
+    RequestHandler<APIGatewayProxyEvent, APIGatewayProxyResult>
+>): Promise<Context> => {
+    const req = event;
+    let dataSources: DataSources | null = null;
+    const context = {
+        get dataSources() {
+            if (dataSources === null)
+                throw new Error(
+                    'DataSources are not available during DataSource initialization.'
+                );
+            return dataSources;
+        },
+        request: { req },
+    };
+    dataSources = createDataSources(context);
+    return context;
+};
+
+export const createStandaloneContext = async ({
+    req,
+}: {
+    req: Request;
+}): Promise<Context> => {
     let dataSources: DataSources | null = null;
     const context = {
         get dataSources() {
--- src/dev.ts
+++ src/dev.ts
@@ -1,15 +1,12 @@
 import { startStandaloneServer } from '@apollo/server/standalone';
 import config from './config.js';
-import { createContext } from './context.js';
+import { createStandaloneContext } from './context.js';
 import server from './server.js';
 
-const { url } = await startStandaloneServer(
-    server,
-    {
-        context: createContext,
-        listen: {
-            port: config.PORT,
-        },
+const { url } = await startStandaloneServer(server, {
+    context: createStandaloneContext,
+    listen: {
+        port: config.PORT,
     },
-);
+});
 console.log(`Server started at ${url}`);
