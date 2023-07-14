08478dcfb46aabfa4566c51bb57b2352dd97192f
josephburgess <josephburgess@gmail.com>
2023-07-14 09:09:37 +0100

    move api check into context

--- src/context.ts
+++ src/context.ts
@@ -1,8 +1,13 @@
-import { createDataSources, DataSources } from './datasources/index.js';
+import handleApiKey from './util/handleApiKey.js';
 import { IncomingHttpHeaders } from 'http';
-
+import { DataSources, createDataSources } from './datasources/index.js';
+import { Organisation } from '@prisma/client';
 interface Request {
     headers: IncomingHttpHeaders;
+}
+
+interface Auth {
+    organisation: Organisation;
 }
 
 export type Context = {
@@ -10,10 +15,27 @@ export type Context = {
     request: {
         req: Request;
     };
+    auth: Auth | null;
 };
 
-export const createContext = async ({ req }: { req: Request }): Promise<Context> => {
+export const createContext = async ({
+    req,
+}: {
+    req: Request;
+}): Promise<Context> => {
     let dataSources: DataSources | null = null;
+    let auth: Auth | null = null;
+    if (req.headers['x-api-key']) {
+        const apiKey = req.headers['x-api-key'] as string;
+        const organisation = await handleApiKey(apiKey);
+        if (organisation) {
+            console.log('Valid API key found for: ', organisation.name);
+            auth = {
+                organisation: organisation,
+            };
+        }
+    }
+
     const context = {
         get dataSources() {
             if (dataSources === null)
@@ -23,7 +45,9 @@ export const createContext = async ({ req }: { req: Request }): Promise<Context>
             return dataSources;
         },
         request: { req },
+        auth,
     };
+
     dataSources = createDataSources(context);
     return context;
 };
