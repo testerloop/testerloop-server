1d4e60bc634a17c0061cd4adc527f6ece5298eda
josephburgess <josephburgess@gmail.com>
2023-07-19 16:41:04 +0100

    refactor code

--- src/util/checkRunStatus.ts
+++ src/util/checkRunStatus.ts
@@ -6,41 +6,36 @@ export async function checkS3ResultsExistAndGetData(
     testRunId: string,
     testExecutionId: string,
     dataSources: DataSources,
-) {
-    const fileExists = await dataSources.testResults.doResultsExist(
-        `${testRunId}/${testExecutionId}`,
-    );
+): Promise<Results | null> {
+    const s3Path = `${testRunId}/${testExecutionId}`;
+    const fileExists = await dataSources.testResults.doResultsExist(s3Path);
 
-    if (fileExists) {
-        return await dataSources.testResults.getById(
-            `${testRunId}/${testExecutionId}`,
-        );
+    if (!fileExists) {
+        return null;
     }
 
-    return null;
+    return await dataSources.testResults.getById(s3Path);
 }
 
-export function getRunStatusAndOutcome(testResults: Results) {
-    let runStatus;
-    let testStatus;
-    const testName = testResults.runs[0].tests[0].title.slice(-1)[0] ?? '';
-
-    if (testResults) {
-        if (testResults.status === 'finished') {
-            runStatus = RunStatus.Completed;
-            testStatus = testResults.runs[0].tests.every(
-                (test: Test) => test.state === 'passed',
-            )
-                ? TestStatus.Passed
-                : TestStatus.Failed;
-        } else {
-            runStatus = RunStatus.Running;
-            testStatus = TestStatus.Pending;
-        }
-    } else {
-        runStatus = RunStatus.Queued;
-        testStatus = TestStatus.Pending;
+export function getRunStatusAndOutcome(testResults: Results | null) {
+    if (!testResults) {
+        return {
+            runStatus: RunStatus.Queued,
+            testStatus: TestStatus.Pending,
+            testName: '',
+        };
     }
+
+    const testName = testResults.runs[0]?.tests[0]?.title.slice(-1)[0] ?? '';
+    const allTestsPassed = testResults.runs[0]?.tests.every(
+        (test: Test) => test.state === 'passed',
+    );
+
+    const runStatus =
+        testResults.status === 'finished'
+            ? RunStatus.Completed
+            : RunStatus.Running;
+    const testStatus = allTestsPassed ? TestStatus.Passed : TestStatus.Failed;
 
     return { runStatus, testStatus, testName };
 }
