ef65ea56a3bae32984fca302b8c9a76fb53f3f66
josephburgess <josephburgess@gmail.com>
2023-08-21 12:21:38 +0100

    add getuser method to repo

--- src/repository/User.ts
+++ src/repository/User.ts
@@ -7,6 +7,12 @@ class UserRepository extends PrismaRepository {
         const newUser = this.db.prisma.user.create({ data });
         return newUser;
     }
+    async getUserByEmail(email: string) {
+        const user = this.db.prisma.user.findUnique({
+            where: { email },
+        });
+        return user;
+    }
 }
 
 export default UserRepository;
