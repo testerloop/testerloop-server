e3bfb2ec482a345538b37eaa7f49a3e50c9e4860
josephburgess <josephburgess@gmail.com>
2023-09-12 10:24:04 +0100

    move context to express middleware

--- src/dev.ts
+++ src/dev.ts
@@ -1,12 +1,13 @@
 import http from 'http';
 
-import express from 'express';
+import express, { Request } from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import { expressMiddleware } from '@apollo/server/express4';
 
 import config from './config.js';
 import { createApolloServer } from './server.js';
+import { Context, createContext } from './context.js';
 
 async function startExpressServer() {
     try {
@@ -14,17 +15,21 @@ async function startExpressServer() {
         const httpServer = http.createServer(app);
 
         const server = createApolloServer(httpServer);
-
+        const context = {
+            context: async ({ req }: { req: Request }): Promise<Context> => {
+                return createContext({ req });
+            },
+        };
         await server.start();
 
         app.use(cors());
         app.use(bodyParser.json());
-        app.use('/graphql', expressMiddleware(server));
+        app.use('/api', expressMiddleware(server, context));
 
         await new Promise<void>((resolve) =>
             httpServer.listen({ port: config.PORT }, () => {
                 console.log(
-                    `Server started at http://localhost:${config.PORT}/graphql`,
+                    `Server started at http://localhost:${config.PORT}/api`,
                 );
                 resolve();
             }),
--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,11 @@ import http from 'http';
 import { ApolloServer } from '@apollo/server';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import { makeExecutableSchema } from '@graphql-tools/schema';
-import { Request } from 'express';
 import { WebSocketServer } from 'ws';
 import { useServer } from 'graphql-ws/lib/use/ws';
 
 import schema from './schema.js';
 import resolvers from './resolvers/index.js';
-import { createContext, Context } from './context.js';
 
 export function createApolloServer(httpServer: http.Server) {
     const executableSchema = makeExecutableSchema({
@@ -19,16 +17,13 @@ export function createApolloServer(httpServer: http.Server) {
 
     const wsServer = new WebSocketServer({
         server: httpServer,
-        path: '/graphql',
+        path: '/api',
     });
 
     const wsCleanup = useServer({ schema: executableSchema }, wsServer);
 
-    const apolloConfig = {
+    return new ApolloServer({
         schema: executableSchema,
-        context: async ({ req }: { req: Request }): Promise<Context> => {
-            return createContext({ req });
-        },
         plugins: [
             ApolloServerPluginDrainHttpServer({ httpServer }),
             {
@@ -41,7 +36,5 @@ export function createApolloServer(httpServer: http.Server) {
                 },
             },
         ],
-    };
-
-    return new ApolloServer(apolloConfig);
+    });
 }
